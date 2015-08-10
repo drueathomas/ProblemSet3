a5fd3a76686c3af9ca5584b78132601eee92ecb1
Drue Thomas <Julie@Drues-MacBook-Pro.local>
2015-08-09 22:46:26 -0400

    Initial Commit

---
+++ Shape/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Shape
+//
+//  Created by Drue Thomas on 8/9/15.
+//  Copyright (c) 2015 Drue Thomas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
